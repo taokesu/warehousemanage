5f4ed2ffacca0f1ad349e86ae4b0733eda39375f
shkola.denis0 <shkola.denis0@gmail.com>
2025-11-09 18:53:42 +0000

    Font fix

--- inventory/utils.py
+++ inventory/utils.py
@@ -1,29 +1,53 @@
+import os
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
+from django.conf import settings
 from xhtml2pdf import pisa
+# ИМПОРТ ДЛЯ ПОИСКА СТАТИЧЕСКИХ ФАЙЛОВ
+from django.contrib.staticfiles import finders
+
+def link_callback(uri, rel):
+    """
+    Преобразует URI в абсолютный путь для xhtml2pdf,
+    чтобы найти статические файлы (шрифты, CSS, изображения).
+    """
+    # uri == /static/fonts/DejaVuSans.ttf
+    # sUrl = /static/
+    sUrl = settings.STATIC_URL
+    
+    # Преобразуем в относительный путь: fonts/DejaVuSans.ttf
+    static_path = uri.replace(sUrl, '')
+    
+    # Ищем абсолютный путь к файлу с помощью Django
+    result = finders.find(static_path)
+
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        # Берем первый найденный путь
+        path = result[0]
+        return path
+    
+    return uri # Возвращаем исходный URI, если ничего не найдено
 
 def render_to_pdf(template_src, context_dict={}):
     """
     Функция для рендеринга HTML-шаблона в PDF-документ.
-
-    :param template_src: Путь к HTML-шаблону.
-    :param context_dict: Словарь с контекстом для шаблона.
-    :return: HttpResponse с PDF-документом или None в случае ошибки.
     """
     template = get_template(template_src)
     html = template.render(context_dict)
     
-    # Создаем байтовый поток для PDF
     result = BytesIO()
     
-    # Генерируем PDF
-    # Мы используем кодировку UTF-8 для поддержки кириллицы
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    # ИЗМЕНЕНО: Генерируем PDF, передавая link_callback
+    pdf = pisa.pisaDocument(
+        BytesIO(html.encode("UTF-8")), 
+        dest=result,
+        link_callback=link_callback
+    )
     
-    # Если ошибок нет, возвращаем PDF как HTTP-ответ
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
         
-    # В случае ошибки возвращаем None
     return None
