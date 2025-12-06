10059aebbc39c1bbddbcacc671e78ff3900239c4
shkola.denis0 <shkola.denis0@gmail.com>
2025-12-06 22:21:10 +0000

    fix

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stock/', views.stock_list, name='stock_list'),
-    path('incoming/', views.incoming_transaction_view, name='incoming_transaction'),
-    path('outgoing/', views.outgoing_transaction_view, name='outgoing_transaction'),
+    # path('stock/', views.stock_list, name='stock_list'),
+    # path('incoming/', views.incoming_transaction_view, name='incoming_transaction'),
+    # path('outgoing/', views.outgoing_transaction_view, name='outgoing_transaction'),
     path('documents/', views.document_list, name='document_list'),
     path('documents/<int:document_id>/', views.document_detail, name='document_detail'),
-    path('documents/<int:document_id>/pdf/', views.document_pdf_view, name='document_pdf'), # Новый URL для PDF
-    path('incoming/create/', views.incoming_transaction_view, name='incoming_transaction_create'),
-    path('outgoing/create/', views.outgoing_transaction_view, name='outgoing_transaction_create'),
+    # path('documents/<int:document_id>/pdf/', views.document_pdf_view, name='document_pdf'), # Новый URL для PDF
+    # path('incoming/create/', views.incoming_transaction_view, name='incoming_transaction_create'),
+    # path('outgoing/create/', views.outgoing_transaction_view, name='outgoing_transaction_create'),
 ]
