9c4ced2843a2bd6ea5bba0754ce8115d5c3111dc
shkola.denis0 <shkola.denis0@gmail.com>
2025-12-11 20:51:43 +0000

    fix

--- reports/urls.py
+++ reports/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.report_list, name='report_list'),
     path('stock/', views.stock_report, name='stock_report'),
     path('low-stock/', views.low_stock_report, name='low_stock_report'),
-    path('sales/', views.sales_report, name='sales_report'),
-    path('turnover/', views.product_turnover_report, name='product_turnover_report'),
+    path('sales/', views.sales_profitability_report, name='sales_profitability_report'),
+    path('turnover/', views.inventory_turnover_report, name='inventory_turnover_report'),
 ]
