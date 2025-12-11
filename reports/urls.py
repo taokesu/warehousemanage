ea744c9b37771b12d0587175c8f877cd601dc71b
shkola.denis0 <shkola.denis0@gmail.com>
2025-12-11 20:29:04 +0000

    fix

--- reports/urls.py
+++ reports/urls.py
@@ -1,11 +1,10 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.report_list, name='report_list'),
     path('stock/', views.stock_report, name='stock_report'),
-    path('restock/', views.restock_needed_report, name='restock_needed_report'),
+    path('low-stock/', views.low_stock_report, name='low_stock_report'),
     path('sales/', views.sales_report, name='sales_report'),
     path('turnover/', views.product_turnover_report, name='product_turnover_report'),
 ]
